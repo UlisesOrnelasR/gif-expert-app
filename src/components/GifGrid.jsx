938d8596757fe8f3f7291db634861e514cc03d76
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 15:21:48 -0600

    Mostrando titulo de los gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola mundo</p>
+      <ol>
+        {images.map((image) => (
+          <li key={image.id}>{image.title}</li>
+        ))}
+        <li>titulo</li>
+      </ol>
     </>
   );
 };
