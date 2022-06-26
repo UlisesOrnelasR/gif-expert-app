ab3c5dc5a379170c2ef27567425a5786b2e1eca3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 16:01:14 -0600

    Checando que funcione la categoria por default

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One Puch"]);
+  const [categories, setCategories] = useState(["Dogs"]);
   const onAddCategory = (newCategory) => {
     if (categories.includes(newCategory)) {
       return;
