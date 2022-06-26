8aef0bcfb2d47577d308ca3ea469e3d454adffda
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-26 16:46:31 -0600

    Creando archivo de barril, index.js para no hacer muchas importaciones en los archivos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components/index.js";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Dogs"]);
