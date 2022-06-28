1fce269d384ef590a4e47dc7afcd329c1ed31008
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-06-27 22:33:41 -0600

    Terminando pruebas al componente <GifItem />

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,11 +1,22 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe("Pruebass sobre el componente <GifItem />", () => {
   const title = "Naruto";
-  const url = "https://www.naruto-academy.com";
+  const url = "https://naruto.com";
   test("Deberia de hacer match con el snapshot", () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+  test("Deberia mostrar la imagen con el URL y el ALT indicado", () => {
+    render(<GifItem title={title} url={url} />);
+    // expect(screen.getByRole("img").alt).toBe(title);
+    const { alt } = screen.getByRole("img");
+    expect(alt).toBe(alt);
+  });
+  test("Debe de mostrar el titulo en el componente", () => {
+    render(<GifItem title={title} url={url} />);
+
+    expect(screen.getByText(title)).toBeTruthy;
   });
 });
