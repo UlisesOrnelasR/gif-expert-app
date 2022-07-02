1aa6ecf57a5eedeb8d70b74b9bf7b33eab46c72e
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-02 13:12:03 -0600

    Pruebas sobre componente <GifGrid />

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,12 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
+jest.mock("../../src/hooks/useFetchGifs");
 describe("Pruebas en <GifGrid />", () => {
   const category = "Dogs";
 
   test("Debe de mostrar el loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
     render(<GifGrid category={category} />);
     expect(screen.getByText("Cargando...."));
     expect(screen.getByText(category));
+  });
+  test("Debe de mostrar items cuando se cargan las imagenes useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Naruto",
+        url: "https://www.hals.coms",
+      },
+      {
+        id: "Dogs",
+        title: "Los animales",
+        url: "https://www.pitbull.coms",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
   });
 });
